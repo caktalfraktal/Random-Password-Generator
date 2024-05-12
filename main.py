86d36362d47f46d3c2ab036367513d1ecab7f5d6
caktalfraktal <cirelli@gmail.com>
2024-05-11 20:49:27 -0400

    Updated to do memory cleanup

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import secrets
 import string
 
 def generate_password():
-    password = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(12))
-    password_entry.delete(0, tk.END)  # Clear the entry field
-    password_entry.insert(0, password)  # Insert the new password
+    try:
+        password = ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(12))
+        password_entry.delete(0, tk.END)  # Clear the entry field
+        password_entry.insert(0, password)  # Insert the new password
+    finally:
+        password = ' ' * len(password) # Overwrite the password variable before exiting
 
 # Create the main window
 root = (tk.Tk)()
